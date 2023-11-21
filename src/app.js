94ed83184ca2de6766927c4b0ed998293494a710
Nha Nguyen <mrbjnluntit@gmail.com>
2023-11-22 02:46:30 +0700

    Add Multer to implement uploading

--- src/app.js
+++ src/app.js
@@ -3,8 +3,22 @@ const cors = require("cors");
 const productsRouter = require("./routes/products.router");
 const ordersRouter = require("./routes/orders.router");
 const cartsRouter = require("./routes/carts.router");
+const multer = require("multer");
 
 const app = express();
+
+//Multer storage
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, './src/images');
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.originalname)
+    }
+});
+
+//Load multer
+const upload = multer({ storage:storage });
 
 const {
   resourceNotFound,
@@ -23,7 +37,12 @@ app.use("/api/orders", ordersRouter);
 app.use("/api/carts", cartsRouter);
 
 // Access the images folder
-app.use('/images', express.static(__dirname + 'images'));
+app.use('/api/images', express.static(__dirname + '/images'));
+
+//Multer handler for uploading file
+app.post("/api/upload", upload.single("Uploadfile"), (req, res, next) => {
+  res.json({ file: req.file});
+})
 
 // Handle 404 response
 app.use(resourceNotFound);
